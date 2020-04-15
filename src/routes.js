2404f712ce8474112fc82cbd950d7b6e4d00b2d7
maxwneto <maxwneto@gmail.com>
2020-04-15 08:48:05 -0300

    instalado biblioteca multer afim de tratar a manipulação de multi-arquivos. Criado arquivo src>config>multer.js para tratamento da nomenclatura e local de armazenando do mesmo

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // rota para validação de usuário
 routes.post('/sessions', SessionController.store);
@@ -19,6 +23,10 @@ routes.use(authMiddleware);
 
 // rota para update de usuário
 routes.put('/users/', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: 'true' });
+});
 
 export default routes;
 
